f665442505ad9aa4c03bc4951e54a0ae90b4f09a
Reece Holmdahl <rholmdahl@gmail.com>
2018-08-30 19:01:57 -0500

    Test Ben commit

--- src/main/java/frc/team2220/robot/RobotConstants.java
+++ src/main/java/frc/team2220/robot/RobotConstants.java
@@ -1,13 +1,18 @@
 package frc.team2220.robot;
 
-public class RobotConstants {
+public class RobotConstants<b> {
 
     // CAN Talon IDs
-    static final int    LEFT_DRIVE_MASTER   = 1,
-                        LEFT_DRIVE_SLAVE    = 3;
+    static final int LEFT_DRIVE_MASTER = 1,
+            LEFT_DRIVE_SLAVE = 3;
 
-    static final int    RIGHT_DRIVE_MASTER  = 2,
-                        RIGHT_DRIVE_SLAVE   = 4;
+    static final int RIGHT_DRIVE_MASTER = 2,
+            RIGHT_DRIVE_SLAVE = 4;
 
-    // Comment for test ben commit part 2
+    void test() {
+        int a = 5, b = 13, c = 21;
+        if ((a + 8) == b) {
+            c = 15;
+        }
+    }
 }
