177392f069c5e62a8d2e311d0bae5033c2066a6c
Reece Holmdahl <rholmdahl@gmail.com>
2018-09-06 20:53:20 -0500

    Merge tests. Set Shooter and Pneumatics ID to 0 for TBD
    Signed-off-by: Reece Holmdahl <rholmdahl@gmail.com>

--- src/main/java/frc/team2220/robot/RobotMap.java
+++ src/main/java/frc/team2220/robot/RobotMap.java
@@ -11,6 +11,18 @@ public class RobotMap {
             RIGHT_DRIVE_MASTER = 3,
             RIGHT_DRIVE_SLAVE = 5;
 
+    // Shooter Motor Controllers TODO TBD ID Values
+    public static final int
+            SHOOTER_TOP_RIGHT = 0,
+            SHOOTER_TOP_LEFT = 0,
+            SHOOTER_BOTTOM_RIGHT = 0,
+            SHOOTER_BOTTOM_LEFT = 0;
+
+    // Pneumatics Controllers TODO TBD ID Values
+    public static final int
+            CUBE_PISTON_UP = 0,
+            CUBE_PISTON_DOWN = 0;
+
     // Xbox Controller IDs
     public static final int
             XBOX_DRIVER = 0,
