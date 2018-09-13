ed75020a7cde646f3ba64a58c334bc1b164d90c8
Reece Holmdahl <rholmdahl@gmail.com>
2018-09-13 18:38:49 -0500

    Big merges
    Signed-off-by: Reece Holmdahl <rholmdahl@gmail.com>

--- src/main/java/frc/team2220/robot/OI.java
+++ src/main/java/frc/team2220/robot/OI.java
@@ -5,14 +5,13 @@ import frc.team2220.robot.utils.TwilightXboxController;
 import frc.team2220.robot.commands.ShootCube;
 import frc.team2220.robot.triggers.ControllerTriggerTrigger;
 
-
 // The OI class specifies what command is ran when an action on the controllers occurs
 // TODO assign actions to the controller binds
 @SuppressWarnings("FieldCanBeLocal")
 public class OI {
 
     //Creates instance of ControllerTriggerTrigger object
-    private final ControllerTriggerTrigger shootTrigger;
+    //private final ControllerTriggerTrigger shootTrigger;
 
     // Instantiate (Twilight)XboxController Variables
     private final TwilightXboxController
@@ -32,18 +31,12 @@ public class OI {
         manipulator = new TwilightXboxController(RobotMap.XBOX_MANIPULATOR);
 
         /* ASSIGN COMMANDS TO CONTROLLER BINDS */
-        // Define XboxController Variables
-        driver      = new TwilightXBoxController(RobotMap.XBOX_DRIVER);
-        manipulator= new TwilightXBoxController(RobotMap.XBOX_MANIPULATOR);
-        shootTrigger = new ControllerTriggerTrigger(manipulator, GenericHID.Hand.kRight);
-
-        /* ASSIGN COMMANDS TO CONTROLLER BINDS */
 
         //Configures what button does what
         //Dhruv Changed:
         //TODO Get rid of ControlTriggerTrigger, and use an instance of abstract class Trigger and use simple conditional instead(Reece's Idea).
 
-        shootTrigger.whileActive(new ShootCube(0.24));
-        shootTrigger.whenInactive(new ShootCube(0));
+        //shootTrigger.whileActive(new ShootCube(0.24));
+        //shootTrigger.whenInactive(new ShootCube(0));
     }
 }
